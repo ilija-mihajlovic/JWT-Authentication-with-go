2a13e46e38b09e5af9d9c87a98ae6ed21c30aec6
Ilija Mihajlovic <iilija879a@gmail.com>
2022-11-21 18:38:58 +0100

    setting things up

---
+++ routes/userRuter.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	controller "JWT-Authentication-with-go/controllers"
+	middleware "JWT-Authentication-with-go/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+}
